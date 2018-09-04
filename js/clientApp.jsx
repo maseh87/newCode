0efdd06071f7c6aae319e8ec5090ab407b3840ea
Mason Hargrove <maseh87@gmail.com>
2018-09-04 12:10:34 -0400

    jsx

--- js/clientApp.jsx
+++ js/clientApp.jsx
@@ -3,20 +3,24 @@ import { render } from 'react-dom'
 
 
 const ce = React.createElement;
-
+//props are what is being passed from MyFirstComponent
 const MyTitle = function(props) {
-  return ce('div', null, ce('h1', { style: { color: props.color } }, props.title));
+  // return ce('div', null, ce('h1', { style: { color: props.color } }, props.title));
+  // below is equivalent to the above commented out code
+  return (
+    <div>
+      <h1 style={ {color: props.color} }>{props.title}</h1>
+    </div>
+  )
 };
 
 const MyFirstComponent = function() {
-  return ce(
-    'div',
-    { id: 'my-first-component' },
-    ce(MyTitle, { title: 'Game of Thrones', color: 'YellowGreen' }),
-    ce(MyTitle, { title: 'Stranger Things', color: 'GreenYellow' }),
-    ce(MyTitle, { title: 'Rick and Morty', color: 'peru' }),
-    ce(MyTitle, { title: 'Silicon Valley', color: 'LimeGreen' })
-  );
+  return <div id="my-first-component">
+    <MyTitle title="Game of Thrones" color="YellowGreen" />
+    <MyTitle title="Stanger Things" color="GreenYellow" />
+    <MyTitle title="Rick and Morty" color="LimeGreen" />
+    <MyTitle title="Silicon Valley" color="peru" />
+  </div>
 };
 
 render(ce(MyFirstComponent), document.getElementById('app'));
